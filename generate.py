1ea2c8e5454c65d8c0e168e795f96b8e1fecb5b1
Jeong Arm <kjwonmail@gmail.com>
2022-04-20 01:35:08 +0900

    Convert to python3

--- generate.py
+++ generate.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 """
@@ -16,9 +16,6 @@ Based on:
 import os
 import re
 import sys
-
-reload(sys)
-sys.setdefaultencoding('UTF8')
 
 import fontforge
 import psMat
@@ -46,7 +43,7 @@ font = fontforge.open('vendor/comic-shanns.otf')
 ref = fontforge.open('vendor/Cousine-Regular.ttf')
 for g in font.glyphs():
     uni = g.unicode
-    category = unicodedata.category(unichr(uni)) if 0 <= uni <= sys.maxunicode else None
+    category = unicodedata.category(chr(uni)) if 0 <= uni <= sys.maxunicode else None
     if g.width > 0 and category not in ['Mn', 'Mc', 'Me']:
         target_width = 510
         if g.width != target_width:
@@ -61,7 +58,7 @@ font.comment = 'https://github.com/dtinth/comic-mono-font'
 font.copyright = 'https://github.com/dtinth/comic-mono-font/blob/master/LICENSE'
 
 adjust_height(font, ref, 0.875)
-font.sfnt_names = [] # Get rid of 'Prefered Name' etc.
+font.sfnt_names = []  # Get rid of 'Prefered Name' etc.
 font.fontname = 'ComicMono'
 font.fullname = 'Comic Mono'
 font.generate('ComicMono.ttf')
@@ -72,3 +69,33 @@ font.fullname = 'Comic Mono Bold'
 font.weight = 'Bold'
 font.changeWeight(32, "LCG", 0, 0, "squish")
 font.generate('ComicMono-Bold.ttf')
+
+font2 = fontforge.open('vendor/comic-shanns-v2.otf')
+for g in font2.glyphs():
+    uni = g.unicode
+    category = unicodedata.category(chr(uni)) if 0 <= uni <= sys.maxunicode else None
+    if g.width > 0 and category not in ['Mn', 'Mc', 'Me']:
+        target_width = 510
+        if g.width != target_width:
+            delta = target_width - g.width
+            g.left_side_bearing += delta / 2
+            g.right_side_bearing += delta - g.left_side_bearing
+            g.width = target_width
+
+font2.familyname = 'Comic Mono v2'
+font2.version = '0.1.1'
+font2.comment = 'https://github.com/dtinth/comic-mono-font'
+font2.copyright = 'https://github.com/dtinth/comic-mono-font/blob/master/LICENSE'
+
+adjust_height(font2, ref, 0.875)
+font2.sfnt_names = []  # Get rid of 'Prefered Name' etc.
+font2.fontname = 'ComicMonoV2'
+font2.fullname = 'Comic Mono v2'
+font2.generate('ComicMonoV2.ttf')
+
+font2.selection.all()
+font2.fontname = 'ComicMonoV2-Bold'
+font2.fullname = 'Comic Mono v2 Bold'
+font2.weight = 'Bold'
+font2.changeWeight(32, "LCG", 0, 0, "squish")
+font2.generate('ComicMonoV2-Bold.ttf')
